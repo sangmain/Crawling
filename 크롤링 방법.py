9bfc75a7294d46f555cdc1cb55d02d22e1c828a2
sangmin99554 <sangmin99554@naver.com>
2019-09-24 15:05:09 +0900

    crawling summary

---
+++ 크롤링 방법.py
@@ -0,0 +1,13 @@
+'''
+
+1.  pip install selenium
+    pip install bs4
+    pip install requests
+
+2. latest version works only on firefox
+    https://pypi.org/project/selenium/
+    https://github.com/mozilla/geckodriver/releases
+
+3. "add" the repository of geckodriver in "environment path"
+
+'''
